d5a3f09521db8b3ce48bc64533624fbe3dca9881
Eric O <eric@ejdigitalmedia.com>
2024-02-23 15:05:55 -0500

    Switch TransferSource has_one :repository relationship to belongs_to

--- app/models/transfer_source.rb
+++ app/models/transfer_source.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'digest'
 
 class TransferSource < ApplicationRecord
-	has_one :repository, required: false
-	validates :path, presence: { strict: true }, on: :create
+  belongs_to :repository, optional: true
+  validates :path, presence: { strict: true }, on: :create
 
-	validates_with PathValidator, on: :update
-	validates_with PathHashValidator
+  validates_with PathValidator, on: :update
+  validates_with PathHashValidator
 
-	include PathHashes
+  include PathHashes
 end
