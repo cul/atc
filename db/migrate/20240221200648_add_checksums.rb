7c1bc65723d0325d0b45da907459c3bc840a632a
Eric O <eric@ejdigitalmedia.com>
2024-02-26 16:37:31 -0500

    Add chunk_size field to Checksum model (to indicate when we are chunking a file during checksum generation, which would mean a checksum of checksums for that file)

--- db/migrate/20240221200648_add_checksums.rb
+++ db/migrate/20240221200648_add_checksums.rb
@@ -7,10 +7,12 @@ class AddChecksums < ActiveRecord::Migration[7.1]
     end
     add_index(:checksum_algorithms, :name, unique: true)
     add_index(:checksum_algorithms, :empty_value, unique: true)
+
     create_table :checksums do |t|
       t.string :value, null: false
       t.references :checksum_algorithm, null: false
       t.references :transfer_source, null: false
+      t.integer :chunk_size, null: true
       t.timestamps
     end
   end
