b26b0c38983b0b776c06e71ebb825dd690251526
Eric O <eric@ejdigitalmedia.com>
2024-03-01 18:09:39 -0500

    Rename Atc::Utils::HexUtils.unhex to Atc::Utils::HexUtils.hex_to_bin; Add corresponding bin_to_hex method

--- spec/atc/utils/hex_utils_spec.rb
+++ spec/atc/utils/hex_utils_spec.rb
@@ -3,9 +3,22 @@
 require 'rails_helper'
 
 describe Atc::Utils::HexUtils do
-  context '.unhex' do
-    it 'unpacks hex data to binary string per mysql expectations' do
-      expect(described_class.unhex('4D7953514C')).to eql 'MySQL'.b
+  context '.hex_to_bin' do
+    it 'converts hex data to a binary string as expected' do
+      expect(described_class.hex_to_bin('4d7953514c')).to eql 'MySQL'.b
+    end
+  end
+
+  context '.bin_to_hex' do
+    it 'converts a binary string to hex as expected' do
+      expect(described_class.bin_to_hex('MySQL')).to eql '4d7953514c'
+    end
+  end
+
+  context 'round trip conversions' do
+    let(:hex_string) { '7b8ff55800b55c0b3d53d2c81067225b115edfda4572a9253581a31efd231d94' }
+    it 'works as expected' do
+      expect(described_class.bin_to_hex(described_class.hex_to_bin(hex_string))).to eql(hex_string)
     end
   end
 end
