360957fc143864a4759bc49c259e0b471329e05f
Eric O <eric@ejdigitalmedia.com>
2024-02-29 19:07:27 -0500

    Allow path length to be up to 4096 characters (max allowed on Linux filesystem)

--- db/migrate/20240221171125_transfer_sources.rb
+++ db/migrate/20240221171125_transfer_sources.rb
@@ -5,7 +5,7 @@ class TransferSources < ActiveRecord::Migration[7.1]
       t.timestamps
     end
     create_table :transfer_sources do |t|
-      t.string :path, null: false
+      t.string :path, null: false, limit: 4096 # 4096 is the max path length on a linux filesystem
       t.binary :path_hash, limit: 32, null: false
       t.bigint :object_size, null: false
       t.datetime :on_prem_deleted_at, null: true
