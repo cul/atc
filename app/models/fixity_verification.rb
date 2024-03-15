474f78a0ceb1e435bbec0e66ba440eb5bc754504
Eric O <eric@ejdigitalmedia.com>
2024-03-15 11:16:36 -0400

    Remove unnecessary validations in FixityVerification class (from accidental copy/paste)

--- app/models/fixity_verification.rb
+++ app/models/fixity_verification.rb
@@ -5,11 +5,4 @@ class FixityVerification < ApplicationRecord
   belongs_to :stored_object
 
   enum status: { pending: 0, failure: 1, success: 2 }
-
-  # Some db backends don't enforce a limit on binary field length,
-  # so the limit below is meant to ensure that we don't ever
-  # accidentally add a larger value. Make sure to update this
-  # value if the database limit ever changes.
-  validates :transfer_checksum_value, length: { maximum: 4 }
-  validates_with TransferChecksumValidator
 end
