2448f9fae3aae8e2c63b5ca9db17efa660b082db
Nathan Alexander Walker <nwalker2398@yahoo.com>
2024-03-15 12:13:09 -0400

    Finished resolving all rubocop offenses

--- app/jobs/create_pending_transfer_job.rb
+++ app/jobs/create_pending_transfer_job.rb
@@ -1,32 +1,39 @@
 # frozen_string_literal: true
 
-multi_part_required_size = 100.megabytes
+MULTI_PART_REQUIRED_SIZE = 100.megabytes
 
 class CreatePendingTransferJob < ApplicationJob
   queue_as Atc::Queues::CREATE_PENDING_TRANSFER
 
   def perform(source_object_id)
-    # Raises ActiveRecord::RecordNotFound if no SourceObject has that id.
-    source_object = SourceObject.find(source_object_id)
-    file_size = File.size(source_object.file_path)
+    source_object, file_size, whole_file_checksum = calculate_whole_file_checksum source_object_id
 
-    whole_file_checksum = nil
-    File.open(file_path, 'rb') { |file| whole_file_checksum = Digest::CRC32c.digest(file.read) }
-
-    if file_size > multi_part_required_size
+    if file_size > MULTI_PART_REQUIRED_SIZE
       part_size = Aws::S3::MultipartFileUploader.new.send(:compute_default_part_size, file_size)
 
-      multi_part_checksum = calculate_multi_part_checksum part_size
+      multi_part_checksum = calculate_multi_part_checksum(source_object.file_path, part_size)
       PendingTransfer.create(transfer_checksum_algorithm_id: :aws, transfer_checksum_value: multi_part_checksum,
                              transfer_chunk_size: part_size)
     else
       PendingTransfer.create(transfer_checksum_algorithm_id: :aws, transfer_checksum_value: whole_file_checksum,
                              transfer_chunk_size: nil)
     end
-    PendingTransfer.create(transfer_checksum_algorithm_id: :gcp, transfer_checksum_value: whole_file_checksum, transfer_chunk_size: nil)
+    PendingTransfer.create(transfer_checksum_algorithm_id: :gcp, transfer_checksum_value: whole_file_checksum,
+                           transfer_chunk_size: nil)
   end
 
-  def calculate_multi_part_checksum(part_size)
+  def calculate_whole_file_checksum(source_object_id)
+    # Raises ActiveRecord::RecordNotFound if no SourceObject has that id.
+    source_object = SourceObject.find(source_object_id)
+    file_path = source_object.file_path
+    file_size = File.size(file_path)
+
+    whole_file_checksum = nil
+    File.open(file_path, 'rb') { |file| whole_file_checksum = Digest::CRC32c.digest(file.read) }
+    [source_object, file_size, whole_file_checksum]
+  end
+
+  def calculate_multi_part_checksum(file_path, part_size)
     c2c32c_bin_checksums_for_parts = []
     File.open(file_path, 'rb') do |file|
       while (buffer = file.read(part_size))
