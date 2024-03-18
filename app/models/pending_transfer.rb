f7af383e07f2adeaeb99c104ebe8a0cfdb7c9fde
Benjamin Armintor <armintor@gmail.com>
2024-03-17 22:53:19 -0400

    Update pending_transfer.rb
    I think this is intended to match the other checksum field lengths

--- app/models/pending_transfer.rb
+++ app/models/pending_transfer.rb
@@ -11,6 +11,6 @@ class PendingTransfer < ApplicationRecord
   # so the limit below is meant to ensure that we don't ever
   # accidentally add a larger value. Make sure to update this
   # value if the database limit ever changes.
-  validates :transfer_checksum_value, length: { maximum: 4 }
+  validates :transfer_checksum_value, length: { maximum: 64 }
   validates_with TransferChecksumValidator
 end
