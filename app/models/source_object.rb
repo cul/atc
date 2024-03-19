671188f184d846e303e5238b3f48e0117477f96a
Benjamin Armintor <armintor@gmail.com>
2024-03-19 09:49:00 -0400

    give SourceObject an inverse relation for pending_transfers

--- app/models/source_object.rb
+++ app/models/source_object.rb
@@ -6,6 +6,8 @@ class SourceObject < ApplicationRecord
   belongs_to :repository, optional: true
   belongs_to :fixity_checksum_algorithm, class_name: 'ChecksumAlgorithm', optional: true
 
+  has_many :pending_transfers, inverse_of: :source_object
+
   validates :path, :path_hash, presence: { strict: true }, on: :create
   validates :object_size, presence: true
   # Some db backends don't enforce a limit on binary field length,
