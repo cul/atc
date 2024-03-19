02b53bc021531ea9881eb1af25a2f21c770359c4
Eric O <eric@ejdigitalmedia.com>
2024-03-19 18:11:48 -0400

    Disable Style/MultilineIfModifier rule in SourceObjectChecksumValidator class for better readability

--- app/validators/pending_transfer/source_object_checksum_validator.rb
+++ app/validators/pending_transfer/source_object_checksum_validator.rb
@@ -1,22 +1,18 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/MultilineIfModifier
+
 # Ensures that this object's associated source_object has been assigned a fixity_checksum_value.
 class PendingTransfer::SourceObjectChecksumValidator < ActiveModel::Validator
   def validate(record)
     return if record.source_object.nil?
 
-    if record.source_object.fixity_checksum_value.nil?
-      record.errors.add(
-        :source_object,
-        'is missing a fixity_checksum_value (which is required for a transfer)'
-      )
-    end
+    record.errors.add(
+      :source_object, 'is missing a fixity_checksum_value (which is required for a transfer)'
+    ) if record.source_object.fixity_checksum_value.nil?
 
-    if record.source_object.fixity_checksum_algorithm_id.nil?
-      record.errors.add(
-        :source_object,
-        'is missing a fixity_checksum_algorithm (which is required for a transfer)'
-      )
-    end
+    record.errors.add(
+      :source_object, 'is missing a fixity_checksum_algorithm (which is required for a transfer)'
+    ) if record.source_object.fixity_checksum_algorithm_id.nil?
   end
 end
