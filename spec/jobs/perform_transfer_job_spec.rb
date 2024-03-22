b64c9a537dccd18938535cf251d7149296914981
Eric O <eric@ejdigitalmedia.com>
2024-03-22 17:06:20 -0400

    Additional StoredObject existence checks in perform_transfer_job_spec.rb

--- spec/jobs/perform_transfer_job_spec.rb
+++ spec/jobs/perform_transfer_job_spec.rb
@@ -19,6 +19,7 @@ describe PerformTransferJob do
   it 'creates the expected StoredObject record' do
     expect(aws_storage_provider).to receive(:perform_transfer).with(pending_transfer, object_key, expected_tags)
     perform_transfer_job.perform(pending_transfer.id)
+    expect(StoredObject.first.path).to eq(object_key)
   end
 
   it 'does not create a StoredObject record when a StoredObject record already exists '\
@@ -31,37 +32,44 @@ describe PerformTransferJob do
     expect(pending_transfer).to receive(:update).with(status: :failure, error_message: String)
     expect(aws_storage_provider).not_to receive(:perform_transfer).with(pending_transfer, object_key, expected_tags)
     perform_transfer_job.perform(pending_transfer.id)
+    expect(StoredObject.count).to eq(1) # only one StoredObject should exist, not two
   end
 
-  it 'appends a numbered variation to the key when an Atc::Exceptions::ObjectExists error is encountered, '\
-     'and adds an original-path tag' do
-    # The first time that perform_transfer is called, we'll have it raise an exception
-    # to pretend we encountered a key that's in use.
-    allow(aws_storage_provider).to receive(:perform_transfer).with(
-      pending_transfer, object_key, expected_tags
-    ).and_raise(Atc::Exceptions::ObjectExists)
-    # The second time that perform_transfer is called, we expect it to receive a renamed variation
-    # and an 'original-path' tag, and we'll raise an exception again to pretend there's another collision.
-    allow(aws_storage_provider).to receive(:perform_transfer).with(
-      pending_transfer, object_key.sub('.jpg', '_1.jpg'), expected_tags.merge({ 'original-path' => object_key })
-    ).and_raise(Atc::Exceptions::ObjectExists)
-    # The third time that perform_transfer is called, we expect it to receive a different
-    # renamed variation and an 'original-path' tag, and we won't raise an exception.
-    allow(aws_storage_provider).to receive(:perform_transfer).with(
-      pending_transfer, object_key.sub('.jpg', '_2.jpg'), expected_tags.merge({ 'original-path' => object_key })
-    )
-    perform_transfer_job.perform(pending_transfer.id)
+  context 'when an Atc::Exceptions::ObjectExists error is encountered' do
+    before do
+      # The first time that perform_transfer is called, we'll have it raise an exception
+      # to pretend we encountered a key that's in use.
+      allow(aws_storage_provider).to receive(:perform_transfer).with(
+        pending_transfer, object_key, expected_tags
+      ).and_raise(Atc::Exceptions::ObjectExists)
+      # The second time that perform_transfer is called, we expect it to receive a renamed variation
+      # and an 'original-path' tag, and we'll raise an exception again to pretend there's another collision.
+      allow(aws_storage_provider).to receive(:perform_transfer).with(
+        pending_transfer, object_key.sub('.jpg', '_1.jpg'), expected_tags.merge({ 'original-path' => object_key })
+      ).and_raise(Atc::Exceptions::ObjectExists)
+    end
+
+    it 'appends a numbered variation to the key, and adds an original-path tag' do
+      # The third time that perform_transfer is called, we expect it to receive a different
+      # renamed variation and an 'original-path' tag, and we won't raise an exception.
+      expect(aws_storage_provider).to receive(:perform_transfer).with(
+        pending_transfer, object_key.sub('.jpg', '_2.jpg'), expected_tags.merge({ 'original-path' => object_key })
+      )
+      perform_transfer_job.perform(pending_transfer.id)
+      expect(StoredObject.first.path).to eq(object_key.sub('.jpg', '_2.jpg'))
+    end
   end
 
   context 'when a key is encountered that needs remediation' do
     let(:object_key) { '🎃a/🍕b/c  🎉.jpg' }
     let(:expected_remediated_key) { '_a/_b/c___.jpg' }
 
-    it 'automatically remediates invalid characters and creates the expected StoredObject record' do
+    it 'is remediated automatically and the job completes without error' do
       expect(aws_storage_provider).to receive(:perform_transfer).with(
         pending_transfer, expected_remediated_key, expected_tags
       )
       perform_transfer_job.perform(pending_transfer.id)
+      expect(StoredObject.first.path).to eq(expected_remediated_key)
     end
   end
 end
