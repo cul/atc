b4eac2eaea0bb18a638cd95e70a372d2a010c1a0
Eric O <eric@ejdigitalmedia.com>
2024-04-19 18:16:46 -0400

    Minor comment update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "dlst_atc_production"
+  # config.active_job.queue_name_prefix = "atc_production"
 
   config.action_mailer.perform_caching = false
 
