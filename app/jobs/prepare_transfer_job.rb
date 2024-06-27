6157ffd14c86347f78730512f6c12014fc7bcdc2
Fred Duby <fcd1@columbia.edu>
2024-06-27 16:43:30 -0400

    Enable rubocop: first pass

--- app/jobs/prepare_transfer_job.rb
+++ app/jobs/prepare_transfer_job.rb
@@ -1,65 +1,98 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/AbcSize
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/MethodLength
-# rubocop:disable Metrics/PerceivedComplexity:
-# rubocop:disable Style/MethodCallWithoutArgsParentheses
-
 class PrepareTransferJob < ApplicationJob
   queue_as Atc::Queues::PREPARE_TRANSFER
 
+  def aws_pending_transfers(aws_storage_providers,
+                            source_object,
+                            whole_file_crc32c_checksum)
+    aws_pending_transfers = []
+    aws_storage_providers.each do |aws_storage_provider|
+      aws_pending_transfers << PendingTransfer.create!(
+        transfer_checksum_algorithm: crc32c_checksum_algorithm,
+        transfer_checksum_value: whole_file_crc32c_checksum,
+        storage_provider: aws_storage_provider,
+        source_object: source_object
+      )
+    end
+    aws_pending_transfers
+  end
+
+  def aws_pending_transfers_multipart(aws_storage_providers,
+                                      source_object,
+                                      checksum_data)
+    aws_pending_transfers = []
+    aws_storage_providers.each do |aws_storage_provider|
+      aws_pending_transfers << PendingTransfer.create!(
+        transfer_checksum_algorithm: crc32c_checksum_algorithm,
+        transfer_checksum_value: checksum_data[:binary_checksum_of_checksums],
+        transfer_checksum_part_size: checksum_data[:part_size],
+        transfer_checksum_part_count: checksum_data[:num_parts],
+        storage_provider: aws_storage_provider,
+        source_object: source_object
+      )
+    end
+    aws_pending_transfers
+  end
+
+  def gcp_pending_transfers(gcp_storage_providers,
+                            source_object,
+                            whole_file_crc32c_checksum)
+    gcp_pending_transfers = []
+    gcp_storage_providers.each do |gcp_storage_provider|
+      gcp_pending_transfers << PendingTransfer.create!(
+        transfer_checksum_algorithm: crc32c_checksum_algorithm,
+        transfer_checksum_value: whole_file_crc32c_checksum,
+        storage_provider: gcp_storage_provider,
+        source_object: source_object
+      )
+    end
+    gcp_pending_transfers
+  end
+
+  def checksum_data(source_object, calculate_whole_object)
+    @checksum_data ||= Atc::Utils::AwsChecksumUtils.multipart_checksum_for_file(
+      source_object.path,
+      calculate_whole_object: calculate_whole_object
+    )
+  end
+
+  def whole_file_crc32c_checksum(source_object)
+    @whole_file_crc32c_checksum ||= Digest::CRC32c.file(source_object.path).digest
+  end
+
+  def prep_aws_transfers(aws_storage_providers,
+                         source_object,
+                         calculate_whole_object)
+    # AWS uses whole file checksum for smaller files, and multipart checksum for larger files
+    if File.size(source_object.path) < Atc::Constants::DEFAULT_MULTIPART_THRESHOLD
+      aws_pending_transfers(aws_storage_providers, source_object, whole_file_crc32c_checksum(source_object))
+    else
+      aws_pending_transfers_multipart(aws_storage_providers,
+                                      source_object,
+                                      checksum_data(source_object, calculate_whole_object))
+    end
+  end
+
+  def prep_gcp_transfers(gcp_storage_providers,
+                         source_object)
+    gcp_pending_transfers(gcp_storage_providers, source_object, whole_file_crc32c_checksum(source_object))
+  end
+
   def perform(source_object_id, enqueue_successor: true)
     source_object = SourceObject.find(source_object_id)
-    whole_file_crc32c_checksum = nil
     pending_transfers = []
 
     aws_storage_providers = select_storage_providers(source_object, 'aws')
     gcp_storage_providers = select_storage_providers(source_object, 'gcp')
 
     if aws_storage_providers.present?
-      # AWS uses whole file checksum for smaller files, and multipart checksum for larger files
-      if File.size(source_object.path) < Atc::Constants::DEFAULT_MULTIPART_THRESHOLD
-        whole_file_crc32c_checksum ||= Digest::CRC32c.file(source_object.path).digest
-        aws_storage_providers.each do |aws_storage_provider|
-          pending_transfers << PendingTransfer.create!(
-            transfer_checksum_algorithm: crc32c_checksum_algorithm(),
-            transfer_checksum_value: whole_file_crc32c_checksum,
-            storage_provider: aws_storage_provider,
-            source_object: source_object
-          )
-        end
-      else
-        checksum_data ||= Atc::Utils::AwsChecksumUtils.multipart_checksum_for_file(
-          source_object.path,
-          calculate_whole_object: gcp_storage_providers.present?
-        )
-        whole_file_crc32c_checksum = checksum_data[:binary_checksum_of_whole_file]
-        aws_storage_providers.each do |aws_storage_provider|
-          pending_transfers << PendingTransfer.create!(
-            transfer_checksum_algorithm: crc32c_checksum_algorithm(),
-            transfer_checksum_value: checksum_data[:binary_checksum_of_checksums],
-            transfer_checksum_part_size: checksum_data[:part_size],
-            transfer_checksum_part_count: checksum_data[:num_parts],
-            storage_provider: aws_storage_provider,
-            source_object: source_object
-          )
-        end
-      end
+      pending_transfers.concat prep_aws_transfers(aws_storage_providers, source_object,
+                                                  gcp_storage_providers.present?)
     end
 
-    if gcp_storage_providers.present?
-      # GCP always uses whole file checksum
-      whole_file_crc32c_checksum ||= Digest::CRC32c.file(source_object.path).digest
-      gcp_storage_providers.each do |gcp_storage_provider|
-        pending_transfers << PendingTransfer.create!(
-          transfer_checksum_algorithm: crc32c_checksum_algorithm(),
-          transfer_checksum_value: whole_file_crc32c_checksum,
-          storage_provider: gcp_storage_provider,
-          source_object: source_object
-        )
-      end
-    end
+    # GCP always uses whole file checksum
+    pending_transfers.concat prep_gcp_transfers(gcp_storage_providers, source_object) if gcp_storage_providers.present?
 
     return unless enqueue_successor
 
