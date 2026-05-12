f215b5dde242248857736dda7ce7227a97c11b5b
goldsmithb <bg2918@columbia.edu>
2026-05-12 15:47:44 -0400

    implement Backend API route to get current user

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,10 @@ Rails.application.routes.draw do
 
   # S3 Browser API routes
   namespace :api do
-    get '/greeting', to: 's3_browser#greeting', format: 'json'
     get '/buckets', to: 's3_browser#buckets', format: 'json'
     get '/bucket/:bucket/', to: 's3_browser#get_contents_at_prefix_level', format: 'json'
     get '/object/:bucket/', to: 's3_browser#get_object_details', format: 'json'
+    get '/users/_self', to: 'users#_self', format: 'json'
   end
 
   resque_web_constraint = lambda do |request|
