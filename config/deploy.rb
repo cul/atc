939062f03b1b3b81457ac3f1b18e56aed1a9ca0b
goldsmithb <bg2918@columbia.edu>
2026-06-01 14:16:07 -0400

    add nvm config

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,10 +48,10 @@ set :log_level, :info
 # nvm exec 16 ~/.rvm-alma8/bin/rvm example_app_dev do node --version
 # But this does not work:
 # ~/.rvm-alma8/bin/rvm example_app_dev do nvm exec 16 node --version
-# set :nvm_node_version, fetch(:deploy_name) # This NVM alias must exist on the server
-# [:rake, :node, :npm, :yarn].each do |command_to_prefix|
-#   SSHKit.config.command_map.prefix[command_to_prefix].push("nvm exec #{fetch(:nvm_node_version)}")
-# end
+set :nvm_node_version, fetch(:deploy_name) # This NVM alias must exist on the server
+[:rake, :node, :npm, :yarn].each do |command_to_prefix|
+  SSHKit.config.command_map.prefix[command_to_prefix].push("nvm exec #{fetch(:nvm_node_version)}")
+end
 
 # RVM Setup, for selecting the correct ruby version (instead of capistrano-rvm gem)
 set :rvm_ruby_version, fetch(:deploy_name) # This RVM alias must exist on the server
