01e0688bcdb5ad9dfa55ac2ebcd64f87205399af
goldsmithb <bg2918@columbia.edu>
2026-08-13 15:33:06 -0400

    restore commented out code

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,11 +3,11 @@
 AWS_CONFIG = Rails.application.config_for(:aws).deep_symbolize_keys
 
 S3_CLIENT = Aws::S3::Client.new(
-  region: AWS_CONFIG[:aws_region]
-  # credentials: Aws::Credentials.new
-  # AWS_CONFIG[:aws_access_key_id],
-  # AWS_CONFIG[:aws_secret_access_key]
-
+  region: AWS_CONFIG[:aws_region],
+  credentials: Aws::Credentials.new(
+    AWS_CONFIG[:aws_access_key_id],
+    AWS_CONFIG[:aws_secret_access_key]
+  )
 )
 
 # Setting this here to satisfy expectation for standalone call to
