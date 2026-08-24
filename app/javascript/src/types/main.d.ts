a41b81895758f1bdd9d4667aad58e736295aae7e
goldsmithb <bg2918@columbia.edu>
2026-08-24 13:16:32 -0400

    fix type typo and warning

--- app/javascript/src/types/main.d.ts
+++ app/javascript/src/types/main.d.ts
@@ -2,8 +2,8 @@ import '@tanstack/react-table';
 
 // Extends the tanstack table header column to accept a text align attribute
 // https://github.com/TanStack/table/discussions/4439
-declare module '@tanstack/table-core' {
-  interface ColumnMeta<TData extends RowData, TValue> {
+declare module '@tanstack/react-table' {
+  interface ColumnMeta {
     textAlign: 'left' | 'center' | 'right';
   }
 }
