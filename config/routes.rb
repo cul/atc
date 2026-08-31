1f8765928bba06e282d82693960cb7d68372b72e
goldsmithb <bg2918@columbia.edu>
2026-08-31 14:52:05 -0400

    use suspense; render csv export errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 require 'resque/server'
 
-Rails.application.routes.draw do
+Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
