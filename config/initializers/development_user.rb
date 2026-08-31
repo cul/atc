09ff912690db9f8bf9984a1472d16dafc63afff0
goldsmithb <bg2918@columbia.edu>
2026-08-31 10:51:36 -0400

    add developer user and developer admin sign in options

--- config/initializers/development_user.rb
+++ config/initializers/development_user.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
-DEVELOPMENT_USER_CONFIG = {
+DEVELOPMENT_ADMIN_USER_CONFIG = {
   uid: 'development',
   email: 'development@example.com',
-  password: 'development'
+  password: 'development',
+  is_admin: true
+}.freeze
+
+DEVELOPMENT_NON_ADMIN_USER_CONFIG = {
+  uid: 'user',
+  email: 'user@example.com',
+  password: 'user-password',
+  is_admin: false
 }.freeze
