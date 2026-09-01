b9b559b064642fac6c5f3e8de21c8083643b63fa
goldsmithb <bg2918@columbia.edu>
2026-09-01 14:45:17 -0400

    remove redundant field

--- app/javascript/src/features/csv-exports/components/csv-export-details-display.tsx
+++ app/javascript/src/features/csv-exports/components/csv-export-details-display.tsx
@@ -38,7 +38,7 @@ const CsvExportDetailsDisplay = () => {
               }
             />
           )}
-          {(status === 'failure' || status === 'completed_with_errors') && (
+          {exportErrors.length > 0 && (
             <ObjectDetailField
               label="Errors"
               value={
@@ -57,9 +57,6 @@ const CsvExportDetailsDisplay = () => {
             hint="This is a count of the folders and files in the original selection when the CSV Export was ordered. Selected folders are counted once in this figure; the final export will have a record for each child item of any selected folders and will therefore be larger in most cases."
           />
           <ObjectDetailField label="Last Updated" value={formatLastModified(updatedAt)} />
-          {exportErrors.length > 0 && (
-            <ObjectDetailField label="Export Errors" value={exportErrors} />
-          )}
           <ObjectDetailField
             label="Export Paths"
             value={<ExportPathsDisplayTable exportPaths={exportPaths} />}
